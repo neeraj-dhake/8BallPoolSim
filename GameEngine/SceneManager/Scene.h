eecdd9ed90dc3c22ca8141d01c15bee1983e72f0
Neeraj Dhake <neerajjd97@gmail.com>
2017-06-06 14:47:42 +0530

    Scene.h update

--- GameEngine/SceneManager/Scene.h
+++ GameEngine/SceneManager/Scene.h
@@ -1,21 +1,24 @@
 #pragma once
 #include "../GameplayEngine/IWorldObject.h"
+#include "../GameplayEngine/Vector3D.h"
 #include <list>
 class SceneManager;
 
 class Scene {
 protected:
-	unsigned int NameHash;
 	bool is_active;
 	SceneManager* Manager;
 	std::list<IWorldObject*> objects_in_scene;
+	Vector3D camera_position;
+	Vector3D camera_lookat;
+	Vector3D camera_up;
 public:
 	Scene();
-	bool IsActive();
-	void SetActive(bool);
-	void SetManager(SceneManager*);
-	void AddObject(IWorldObject*);
-	void DeleteObject(IWorldObject*);
+	virtual bool IsActive() = 0;
+	virtual void SetActive(bool) = 0;
+	virtual void SetManager(SceneManager*) = 0;
+	virtual void AddObject(IWorldObject*) = 0;
+	virtual void DeleteObject(IWorldObject*) = 0;
 	~Scene();
 };
 
