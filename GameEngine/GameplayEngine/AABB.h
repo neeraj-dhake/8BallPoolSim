8f84fef66b677031f429a5fcc22d0e5f4d849868
Neeraj Dhake <neerajjd97@gmail.com>
2017-06-07 15:41:52 +0530

    Merge branch 'feature/scene-manager' into develop

--- GameEngine/GameplayEngine/AABB.h
+++
@@ -1,8 +0,0 @@
-#include "Vector3D.h"
-#pragma once
-struct AABB
-{
-	Vector3D min;
-	Vector3D max;
-
-};
--- GameEngine/InputLib_Win/KeyBoardInput.cpp
+++ GameEngine/InputLib_Win/KeyBoardInput.cpp
@@ -1,64 +1,33 @@
 #include "KeyBoardInput.h"
 
 
-LRESULT CALLBACK process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam, bool* getkey)
+LRESULT CALLBACK process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam,bool* getkey)
 {
-	bool* keys = getkey;
+	bool* keys =getkey;
 
-	switch (message)
+	switch(message)
 	{
-	case WM_KEYDOWN:
-		switch (wParam)
-		{
-		case VK_ESCAPE:
+	case WM_KEYDOWN: 
+		switch (wParam) 
+		{ 
+		case VK_ESCAPE: 
 			DestroyWindow(hwnd);
 			return -1;
 			keys[KEY_ESCAPE] = 1;
-			break;
-			// add other buttons implemntations
-		case 0x57:
-			keys[KEY_W] = 1;
-			break;
-		case 0x41:
-			keys[KEY_A] = 1;
-			break;
-		case 0x53:
-			keys[KEY_S] = 1;
-			break;
-		case 0x44:
-			keys[KEY_D] = 1;
-			break;
-		case VK_SPACE:
-			keys[KEY_SPACEBAR] = 1;
-			break;
+			break; 
+		 // add other buttons implemntations
 
-		default:
-			break;
+
+		default: 
+			break; 
 		}
 		break;
-	case WM_KEYUP:
-		switch (wParam)
-		{
-		case 0x57:
-			keys[KEY_W] = 0;
-			break;
-		case 0x41:
-			keys[KEY_A] = 0;
-			break;
-		case 0x53:
-			keys[KEY_S] = 0;
-			break;
-		case 0x44:
-			keys[KEY_D] = 0;
-			break;
-		case VK_SPACE:
-			keys[KEY_SPACEBAR] = 0;
-			break;
-
-
-		default:
-			break;
-		}
+	case WM_KEYUP: 
+		/*switch (wParam) 
+		{ 
+		default: 
+		break; 
+		}*/
 		break;
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
@@ -77,8 +46,6 @@ LRESULT CALLBACK process(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam, 
 KeyBoardInput::KeyBoardInput()
 {
 	keys = new bool[Num];
-	for (size_t i = 0; i < Num; i++)
-		keys[i] = false;
 }
 KeyBoardInput::~KeyBoardInput(void)
 {
@@ -88,13 +55,13 @@ KeyBoardInput::~KeyBoardInput(void)
 bool KeyBoardInput::handle(MSG &msg)
 {
 	bool quit = false;
-	while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-		if (process(msg.hwnd, msg.message, msg.wParam, msg.lParam, keys) == -1)	 // normally DefWindowProc is zero
-			return false;
-	}
+	while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+			
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			if(process(msg.hwnd,msg.message,msg.wParam,msg.lParam,keys) == -1)	 // normally DefWindowProc is zero
+				return false;
+		}	
 
 	return true;
 }
